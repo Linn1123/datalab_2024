8ee40201992a7eebf6caab9f78abc9dce75904dc
Linn Lett Moe <linnlettmoe@Linns-MacBook-Air.local>
2024-06-14 10:33:09 -0500

    sentiment

--- sentiment.R
+++ sentiment.R
@@ -54,3 +54,40 @@ top10 <- head(word_freq,10)
 
 ggplot(data=top10, aes(x=word, y= n)) +geom_col()
 
+sentimentr <-left_join(sentiments,word_freq, by= 'word')
+
+NP <- sentimentr %>%
+  group_by(sentiment)%>%
+  summarise(total_frequency = sum(n, na.rm = TRUE))%>%
+  spread(key = sentiment, value = total_frequency, fill = 0) 
+  
+NP <- NP %>% mutate(sentimentality=positive-negative)
+
+ggplot(data= NP, aes(x=sentimentality)) + geom_histogram()
+ggplot(data= NP, aes(x=negative, y= positive,color= sentimentality)) + geom_col()
+
+ggplot(data= NP, aes(x=sentimentality)) + geom_bar()
+
+dream <- survey%>%
+   select(first_name, dream) %>%
+  unnest_tokens(words,dream)
+  
+dream <- dream%>%
+  group_by(words)%>%
+  tally()
+
+wordcloud2(dream)
+
+dream <- dream%>%
+  arrange(desc(n)) 
+
+dreams <- head(dream,16)
+ggplot(data= dreams, aes(x= words, y= n)) + geom_col()
+
+dream1 <- survey%>%
+  select(first_name, dream) %>%
+  unnest_tokens(words,dream)%>%
+  group_by(words,first_name)%>%
+  tally()
+dream1 <- dream1%>%
+  arrange(desc(n)) 
